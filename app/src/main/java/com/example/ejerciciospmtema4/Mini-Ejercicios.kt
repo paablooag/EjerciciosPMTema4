316465cfbe4b2fbee20c0060f9479a198b3bbe0d
EAG <pablo.alcalde.garcia@gmail.com>
2023-11-16 18:32:12 +0100

    pito

--- app/src/main/java/com/example/ejerciciospmtema4/Mini-Ejercicios.kt
+++ app/src/main/java/com/example/ejerciciospmtema4/Mini-Ejercicios.kt
@@ -22,9 +22,61 @@ package com.example.ejerciciospmtema4
         println(cadena)
         //Ejercicio12
         println(remplazar("pito chicoa"))
+        //ejercicio13
+//        var a= null
+//        var b = null
+//        var suma= (a?:1) + (b?:1)
+//        var resta=(a?:1) - (b?:1)
+//        var multiplicacion=(a?:1) * (b?:1)
+//        println("$suma   $resta   $multiplicacion")
+
+        //ejercicio14
+        var a= null
+        var b = null
+        var suma= (a?:1) + (b?:1)
+        var resta=(a?:1) - (b?:1)
+        var multiplicacion=(a?:1) * (b?:1)
+        println("$suma   $resta   $multiplicacion")
+
+       println(any("Hola"))
+
+        //e.16,17,18
+        println("introduce dos numeros")
+        var numero1 = readLine()
+        var numero2 = readLine()
+       println(dividir(null, null))
+
+
 
     }
+//ejercicio16,17,18
+    fun dividir(numero1: String?, numero2: String?):Double{
+    var res=0.0
+        if((numero1 is String) and (numero2 is String)){
 
+    var n= numero1?.toInt() ?: 10
+    var n2 = numero2?.toInt() ?: 20
+        if(n<=0 || n2<=0){
+            throw RuntimeException("No se puede dividir entre 0")
+        }else if (n%n2==0){
+            throw ArithmeticException("El divisor es igual a 0")
+        }else{
+            res= (n/n2).toDouble()
+        }
+        }else{
+            throw RuntimeException("pito")
+        }
+        return res
+    }
+
+//ejercicio15
+    fun any(parametro:Any):Int{
+        var res=0
+        if(parametro is String){
+            return parametro.length
+        }
+        return res
+    }
     fun longitud(cadena:String):Int{
         var res=0
         res=cadena.length
